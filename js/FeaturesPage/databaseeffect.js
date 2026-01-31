64b6da7a3b50c0bffa2943b9858f2d97de7d22bb
jOnel <warumfragstdu03@gmail.com>
2026-02-01 00:14:51 +0100

    typing effect at database box

---
+++ js/FeaturesPage/databaseeffect.js
@@ -0,0 +1,32 @@
+const textElement = document.getElementById('typewriter-text');
+    const games = ["Search for Elden Ring...", "Search for GTA V...", "Search for Hollow Knight...", "Search for Cyberpunk 2077..."];
+    let gameIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function type() {
+        const currentGame = games[gameIndex];
+        
+        if (isDeleting) {
+            textElement.textContent = currentGame.substring(0, charIndex - 1);
+            charIndex--;
+        } else {
+            textElement.textContent = currentGame.substring(0, charIndex + 1);
+            charIndex++;
+        }
+
+        let typeSpeed = isDeleting ? 50 : 100;
+
+        if (!isDeleting && charIndex === currentGame.length) {
+            typeSpeed = 2000; 
+            isDeleting = true;
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            gameIndex = (gameIndex + 1) % games.length;
+            typeSpeed = 500;
+        }
+
+        setTimeout(type, typeSpeed);
+    }
+
+type();
