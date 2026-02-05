8133d151e60acea6df320f0e069cff451174c2d2
jOnel <warumfragstdu03@gmail.com>
2026-02-05 21:16:23 +0100

    reveal fix

--- js/global/reveal.js
+++ js/global/reveal.js
@@ -1,9 +1,24 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('active');
-        }
-    });
-}, { threshold: 0.4 });
+document.addEventListener("DOMContentLoaded", function() {
+    const revealElements = document.querySelectorAll(".reveal");
 
-document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
+    const revealOptions = {
+        threshold: 0.15, 
+        
+        rootMargin: "0px 0px -130px 0px"
+    };
+
+    const revealOnScroll = new IntersectionObserver(function(entries, revealOnScroll) {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add("active");
+                revealOnScroll.unobserve(entry.target);
+            }
+        });
+    }, revealOptions);
+
+    revealElements.forEach((el) => {
+        revealOnScroll.observe(el);
+    });
+});
