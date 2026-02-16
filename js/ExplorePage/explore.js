5b3d237903e3a128fc99353a7bb1876c48b6c632
jOnel <warumfragstdu03@gmail.com>
2026-02-16 21:36:44 +0100

    bitte funktioniere NES

--- js/ExplorePage/explore.js
+++ js/ExplorePage/explore.js
@@ -112,14 +112,14 @@ function platformToIconInfo(platformName) {
 
   // NINTENDO
   if (p.includes("nintendo switch 2") || /\bswitch\s*2\b/.test(p) || p.includes("switch2")) return { src: "../assets/platforms/nintendo/switch2.svg", brand: "nintendo", key: "switch2" };
-  if (p.includes("switch" || /\bswitch\s*1\b/.test(p) )) return { src: "../assets/platforms/nintendo/switch1.svg", brand: "nintendo" };
+  if (p.includes("switch") || /\bswitch\s*1\b/.test(p)) return { src: "../assets/platforms/nintendo/switch1.svg", brand: "nintendo" };
   if (p.includes("wii u")) return { src: "../assets/platforms/nintendo/wiiu.svg", brand: "nintendo" };
   if (p.includes("wii")) return { src: "../assets/platforms/nintendo/wii.svg", brand: "nintendo" };
   if (p.includes("virtual boy")) return { src: "../assets/platforms/nintendo/virtualboy.svg", brand: "nintendo" };
   if (p.includes("nintendo 64") || p.includes("n64")) return { src: "../assets/platforms/nintendo/n64.svg", brand: "nintendo" };
   if (p.includes("gamecube")) return { src: "../assets/platforms/nintendo/gcn.svg", brand: "nintendo" };
-  if (p.includes("nintendo entertainment system") || p.includes("famicom") || /\bnes\b/.test(p) || p.includes("family computer")) {return { src: "../assets/platforms/nintendo/nes.svg", brand: "nintendo", key: "nes" };}
-  if (p.includes("super nintendo entertainment system") || p.includes("super nintendo") || p.includes("snes") || p.includes("super famicom")) return { src: "../assets/platforms/nintendo/snes.svg", brand: "nintendo", key: "snes" };
+  if (p.includes("super nintendo") || /\bsnes\b/.test(p) || p.includes("super famicom")) return { src: "../assets/platforms/nintendo/snes.svg", brand: "nintendo", key: "snes" };
+  if ((p.includes("nintendo entertainment system") || /\bnes\b/.test(p) || (p.includes("famicom") && !p.includes("super famicom")) || p.includes("family computer")) && !(p.includes("super nintendo") || /\bsnes\b/.test(p) || p.includes("super famicom"))) return { src: "../assets/platforms/nintendo/nes.svg", brand: "nintendo", key: "nes" };
   if (p.includes("game boy advance") || p.includes("gba")) return { src: "../assets/platforms/nintendo/gba.svg", brand: "nintendo" };
   if (p.includes("game boy color") || p.includes("gbc")) return { src: "../assets/platforms/nintendo/gbcolor.svg", brand: "nintendo" };
   if (p.includes("game boy")) return { src: "../assets/platforms/nintendo/gameboy.svg", brand: "nintendo" };
