7fb55ea1e7faacaf63f07cf56cf06a638f459299
jOnel <warumfragstdu03@gmail.com>
2026-03-12 14:12:52 +0100

    Fix for headline allignments after comment section was added

--- profile/comment.js
+++ profile/comment.js
@@ -1,6 +1,6 @@
 const commentInput = document.querySelector('.comment_input');
 
 commentInput.addEventListener('input', function() {
-    this.style.height = 'auto';              // zurücksetzen
-    this.style.height = this.scrollHeight + 'px'; // passt Höhe an Text an
+    this.style.height = 'auto';              // reset
+    this.style.height = this.scrollHeight + 'px'; // set to scrollHeight
 });
